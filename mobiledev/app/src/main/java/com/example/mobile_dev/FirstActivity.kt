4320d3ab6b1872aa58466818fcb29c48ddb17557
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2022-11-07 01:32:03 +0300

    Add list items

--- mobiledev/app/src/main/java/com/example/mobile_dev/FirstActivity.kt
+++ mobiledev/app/src/main/java/com/example/mobile_dev/FirstActivity.kt
@@ -1,13 +1,26 @@
 package com.example.mobile_dev
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_first)
+
+        val listView: ListView = findViewById(R.id.listView)
+        val catNames = resources.getStringArray(R.array.cat_names)
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, catNames
+        )
+
+        listView.adapter = adapter
+
     }
 
 }
