c62769c445f455a2d5beee54142972dea3e17d62
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2022-11-07 02:17:11 +0300

    Add first page buttons handlers

--- mobiledev/app/src/main/java/com/example/mobile_dev/FirstActivity.kt
+++ mobiledev/app/src/main/java/com/example/mobile_dev/FirstActivity.kt
@@ -1,12 +1,16 @@
 package com.example.mobile_dev
 
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.util.Log
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 
 
 class FirstActivity : AppCompatActivity() {
+
+    private val TAG = "myLogs"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,38 @@ class FirstActivity : AppCompatActivity() {
 
         listView.adapter = adapter
 
+        val buttonHide: Button = findViewById(R.id.button_hide)
+        val buttonToast: Button = findViewById(R.id.button_toast)
+        val buttonChangeLabel: Button = findViewById(R.id.button_set_label)
+        val switchColor: Switch = findViewById(R.id.switch_color)
+
+        buttonHide.setOnClickListener {
+            Log.i(TAG,"Button Hide pressed")
+            listView.isVisible = !listView.isVisible
+        }
+
+        buttonToast.setOnClickListener {
+            Log.i(TAG,"Button Toast pressed")
+            Toast.makeText(applicationContext, "Toast yes!", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonChangeLabel.setOnClickListener() {
+            val labelText: TextView = findViewById(R.id.label_text)
+            val editText: EditText = findViewById(R.id.edit_text)
+
+            labelText.text = editText.text
+        }
+
+        switchColor.setOnCheckedChangeListener { buttonView, isChecked ->
+            val labelText: TextView = findViewById(R.id.label_text)
+
+            if (isChecked) {
+                labelText.setTextColor(Color.GREEN)
+            }
+            else {
+                labelText.setTextColor(Color.GRAY)
+            }
+        }
     }
 
 }
