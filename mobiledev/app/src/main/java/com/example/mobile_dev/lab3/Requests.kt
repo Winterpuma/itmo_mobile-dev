09c8177903a2bced01ef33bedc41e2efaff6f756
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2022-12-20 05:42:23 +0300

    Add requests

--- mobiledev/app/src/main/java/com/example/mobile_dev/lab3/Requests.kt
+++ mobiledev/app/src/main/java/com/example/mobile_dev/lab3/Requests.kt
@@ -1,10 +1,16 @@
 package com.example.mobile_dev.lab3
 
+import com.example.mobile_dev.lab3.models.Transaction
+import com.example.mobile_dev.lab3.models.User
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.runBlocking
 
 class Requests {
@@ -14,11 +20,59 @@ class Requests {
             logger = Logger.DEFAULT
             level = LogLevel.HEADERS
         }
+        install(ContentNegotiation) {
+            json()
+        }
     }
 
-    fun getUser(id: Int): String {
+    fun getAllUsers(): List<User>{
+        return runBlocking {
+            return@runBlocking client.get("$basicAddress/user").body()
+        }
+    }
+
+    fun getUser(id: Int): User {
         return runBlocking {
             return@runBlocking client.get("$basicAddress/user/$id").body()
+        }
+    }
+
+    fun createUser(id: String, money: Int): HttpResponse {
+        return runBlocking {
+            return@runBlocking client.post("$basicAddress/user") {
+                contentType(ContentType.Application.Json)
+                setBody(User(id, money))
+            }
+        }
+    }
+
+    fun deleteUser(id: String): HttpResponse {
+        return runBlocking {
+            return@runBlocking client.delete("$basicAddress/user/$id")
+        }
+    }
+
+    fun addMoney(id: String, amount: Int): User {
+        return runBlocking {
+            return@runBlocking client.put("$basicAddress/user/$id/add/$amount").body()
+        }
+    }
+
+    fun withdrawMoney(id: String, amount: Int): User {
+        return runBlocking {
+            return@runBlocking client.put("$basicAddress/user/$id/withdraw/$amount").body()
+        }
+    }
+
+    fun luckyMoney(id: String, amount: Int): User {
+        return runBlocking {
+            return@runBlocking client.put("$basicAddress/user/$id/withdraw/$amount").body()
+        }
+    }
+
+    fun getAllUserTransactions(id: String): List<Transaction>{
+        return runBlocking {
+            return@runBlocking client.get("$basicAddress/user/$id/transaction").body()
         }
     }
 }
