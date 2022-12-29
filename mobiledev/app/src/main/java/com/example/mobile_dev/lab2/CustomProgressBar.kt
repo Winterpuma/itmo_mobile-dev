d8cb039a19938f7cfcbb821de787d222fb354380
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2022-12-29 17:46:40 +0300

    Add custom progress bar

---
+++ mobiledev/app/src/main/java/com/example/mobile_dev/lab2/CustomProgressBar.kt
@@ -0,0 +1,37 @@
+package com.example.mobile_dev.lab2
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import com.example.mobile_dev.R
+
+class CustomProgressBar(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private val paint: Paint = Paint()
+
+    var progress: Float = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    init {
+        paint.color = ResourcesCompat.getColor(resources, R.color.purple_500, null)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        if (progress == 0f)
+            return
+
+        canvas.drawCircle(
+            width.toFloat()/2,
+            height.toFloat()/2,
+            progress * 0.1F,
+            paint
+        )
+    }
+}
--- mobiledev/app/src/main/java/com/example/mobile_dev/lab2/SecondActivity.kt
+++ mobiledev/app/src/main/java/com/example/mobile_dev/lab2/SecondActivity.kt
@@ -13,6 +13,11 @@ class SecondActivity : AppCompatActivity() {
     private var run1 = AtomicBoolean(false)
     private var run2 = AtomicBoolean(false)
 
+    private val increment1 = 50
+    private val increment2 = 1
+    private val sleep1: Long = 1000
+    private val sleep2: Long = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_second)
@@ -33,9 +38,12 @@ class SecondActivity : AppCompatActivity() {
         val textView1: TextView = findViewById(R.id.textView_thread1)
         val textView2: TextView = findViewById(R.id.textView_thread2)
 
+        val progressBar1: CustomProgressBar = findViewById(R.id.customProgressBar1)
+        val progressBar2: CustomProgressBar = findViewById(R.id.customProgressBar2)
+
         buttonRun.setOnClickListener {
-            runThreadIfNotActive(run1, textView1, 50, 1000)
-            runThreadIfNotActive(run2, textView2, 1, 100)
+            runThreadIfNotActive(run1, textView1, progressBar1, increment1, sleep1)
+            runThreadIfNotActive(run2, textView2, progressBar2, increment2, sleep2)
         }
 
         buttonStop.setOnClickListener { stopThreads() }
@@ -45,6 +53,9 @@ class SecondActivity : AppCompatActivity() {
 
             textView1.text = "0"
             textView2.text = "0"
+
+            progressBar1.progress = 0F
+            progressBar2.progress = 0F
         }
     }
 
@@ -59,13 +70,14 @@ class SecondActivity : AppCompatActivity() {
         run2.compareAndSet(true, false)
     }
 
-    private fun runThreadIfNotActive(runFlag: AtomicBoolean, textView: TextView, increment: Int, sleep: Long) {
+    private fun runThreadIfNotActive(runFlag: AtomicBoolean, textView: TextView, progressBar: CustomProgressBar, increment: Int, sleep: Long) {
         if (!runFlag.get()) {
             runFlag.set(true)
             thread {
                 while (runFlag.get()) {
                     runOnUiThread {
                         textView.text = addToNumber(textView.text.toString(), increment)
+                        progressBar.progress += increment
                     }
                     Thread.sleep(sleep)
                 }
